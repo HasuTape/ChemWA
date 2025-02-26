19dc9dff1027956df08704dc2d35d93c6a2a7b6f
HasuTape <wiktorwozny5555@gmail.com>
2025-02-26 20:44:38 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,3 @@ function goto(file)
 {
     window.location.href = file;
 }
-function eegg()
-{
-    let input = document.getElementById("ee").value;
-    let correctPhrase = "Luzik arbuzik";
-
-    if (input === correctPhrase) {
-        document.getElementById("result").textContent = "✅ Poprawna fraza!";
-    } else {
-        document.getElementById("result").textContent = "❌ Błędna fraza!";
-    }
-}
