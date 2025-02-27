fc3151bf4b32a3fb13a80f1f4e858c2b8c38db25
HasuTape <wiktorwozny5555@gmail.com>
2025-02-27 22:18:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function goto(file)
 {
     window.location.href = file;
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("theme-change");
+    const themeStyle = document.getElementById("theme-style");
+
+    // Sprawdza, jaki motyw jest zapisany w LocalStorage i ustawia go na stronie
+    if (localStorage.getItem("theme") === "dark") {
+        themeStyle.href = "dark.css";
+    } else {
+        themeStyle.href = "light.css";
+    }
+
+    // Obsługa kliknięcia przycisku zmiany motywu
+    themeToggle.addEventListener("click", () => {
+        if (themeStyle.href.includes("light.css")) {
+            themeStyle.href = "dark.css";
+            localStorage.setItem("theme", "dark");
+        } else {
+            themeStyle.href = "light.css";
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
